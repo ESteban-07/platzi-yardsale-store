940c96b202feae7b125471b39e8d03928dbd1517
Esteban Portillo <esteban.07.elias@gmail.com>
2022-11-16 16:41:44 -0600

    Fusión menú mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const burguerMenu = document.querySelector('.burguer-menu');
+const closeButton = document.querySelector('#close-button');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+burguerMenu.addEventListener('click', showMobileMenu);
+
+function showMobileMenu() {
+    mobileMenu.classList.remove('menu-inactive');
+    mobileMenu.classList.add('menu-active');
+}
+
+closeButton.addEventListener('click', closeMobileMenu);
+
+function closeMobileMenu() {
+    mobileMenu.classList.remove('menu-active');
+    mobileMenu.classList.add('menu-inactive');
 }
