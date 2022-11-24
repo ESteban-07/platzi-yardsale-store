76ae8f2b22162a79730a1855ac6bf6e35d44a8d1
Esteban Portillo <esteban.07.elias@gmail.com>
2022-11-24 11:58:05 -0600

    Lista de productos

--- js/main.js
+++ js/main.js
@@ -14,7 +14,13 @@ const yardSaleLogo = document.querySelector('.logo');
 const shoppingTitle = document.querySelector('#shopping-title');
 const mobileMediaQuery = window.matchMedia('(max-width: 640px)');
 
+const cardsContainer = document.querySelector('.cards-container');
+
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', showMobileMenu);
+closeButton.addEventListener('click', closeMobileMenu);
+openShoppingCartBtn.addEventListener('click', toggleShoppingCart);
+exitShoppingCartBtn.addEventListener('click', exitShoppingCart);
 
 function toggleDesktopMenu() {
     const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
@@ -26,19 +32,13 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
-burguerMenu.addEventListener('click', showMobileMenu);
-
 function showMobileMenu() {
     mobileMenu.classList.add('menu-active');
 }
 
-closeButton.addEventListener('click', closeMobileMenu);
-
 function closeMobileMenu() {
     mobileMenu.classList.remove('menu-active');
 }
-
-openShoppingCartBtn.addEventListener('click', toggleShoppingCart);
 
 function toggleShoppingCart() {
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
@@ -65,8 +65,6 @@ function toggleShoppingCartTitle() {
     }
 }
 
-exitShoppingCartBtn.addEventListener('click', exitShoppingCart);
-
 function exitShoppingCart() {
     shoppingCart.classList.add('inactive');
 }
@@ -74,28 +72,94 @@ function exitShoppingCart() {
 const productList = [];
 productList.push({
     name: 'Bike',
-    price: 120,
+    price: 12700,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 productList.push({
-    name: 'LG TV',
-    price: 999.99,
-    image: 'https://reviewed-com-res.cloudinary.com/image/fetch/s--zsAlJ0VN--/b_white,c_limit,cs_srgb,f_auto,fl_progressive.strip_profile,g_center,h_668,q_auto,w_1187/https://reviewed-production.s3.amazonaws.com/1632142368000/LG-8K-QNED99-Hero.jpg',
+    name: 'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
 });
 productList.push({
-    name: 'Macbook Pro',
-    price: 620,
-    image: 'https://www.cnet.com/a/img/resize/085fe019f07e0f866dac5fcfc76f959f56c6eb67/hub/2021/12/06/537f8843-0daf-4de7-a7fa-36ce916c8e70/20211122-2021-16-inch-apple-macbook-pro-01.jpg?auto=webp&fit=crop&height=1200&width=1200',
+    name: 'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
 });
 productList.push({
-    name: 'Samsung S23',
+    name: 'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
+});
+productList.push({
+    name: 'Seat',
     price: 300,
-    image: 'https://www.tuexperto.com/wp-content/uploads/2022/06/estos-son-los-primeros-rumores-que-estan-surgiendo-en-torno-al-samsung-galaxy-s23.jpg',
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
+});
+productList.push({
+    name: 'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
+});
+productList.push({
+    name: 'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602',
+});
+productList.push({
+    name: 'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603',
+});
+productList.push({
+    name: 'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg',
+});
+productList.push({
+    name: 'Phone waterproof bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/510VNwidLHL._AC_SY1000_.jpg',
 });
 
-// Inserting product cards from js to html
+function renderProducts(arr) {
+    for (let product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
 
-const cardsContainer = document.querySelector('.cards-container');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        productImg.setAttribute('alt', product.name);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `$${formatPrice(product.price)}`;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.append(productPrice, productName);
+
+        const productInfoFigure = document.createElement('figure');
+
+        const addToCartIcon = document.createElement('img');
+        addToCartIcon.setAttribute('src', './assets/icons/bt_add_to_cart.svg');
+        addToCartIcon.setAttribute('alt', `add ${product.name} to cart`);
+
+        productInfoFigure.appendChild(addToCartIcon);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
 
 function formatPrice(price) {
     const numDecimalCommaSeparated = (Math.round(price * 100) / 100)
@@ -104,32 +168,3 @@ function formatPrice(price) {
 
     return numDecimalCommaSeparated;
 }
-
-let productCard = '';
-
-productList.forEach((product) => {
-    productCard = `
-                <div class="product-card">
-                    <img
-                        src="${product.image}"
-                        alt="${product.name}"
-                    />
-                    <div class="product-info">
-                        <div>
-                            <p>
-                            $${formatPrice(product.price)}
-                            </p>
-                            <p>${product.name}</p>
-                        </div>
-                        <figure>
-                            <img
-                                src="./assets/icons/bt_add_to_cart.svg"
-                                alt="add ${product.name} to cart"
-                            />
-                        </figure>
-                    </div>
-                </div>
-    `;
-
-    cardsContainer.innerHTML += productCard;
-});
