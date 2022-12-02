c167a17bfb824469d6b53fe5a48fde48704931c0
Esteban Portillo <esteban.07.elias@gmail.com>
2022-12-02 11:42:58 -0600

    item details

--- js/main.js
+++ js/main.js
@@ -16,11 +16,20 @@ const mobileMediaQuery = window.matchMedia('(max-width: 640px)');
 
 const cardsContainer = document.querySelector('.cards-container');
 
+const productDetail = document.querySelector('.product-detail');
+const productDetailCloseBtn = document.querySelector('.product-detail-close');
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', showMobileMenu);
 closeButton.addEventListener('click', closeMobileMenu);
 openShoppingCartBtn.addEventListener('click', toggleShoppingCart);
 exitShoppingCartBtn.addEventListener('click', exitShoppingCart);
+productDetailCloseBtn.addEventListener('click', exitProductDetail);
 
 function toggleDesktopMenu() {
     const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
@@ -69,56 +78,80 @@ function exitShoppingCart() {
     shoppingCart.classList.add('inactive');
 }
 
+function exitProductDetail() {
+    productDetail.classList.add('inactive');
+}
+
 const productList = [];
 productList.push({
     name: 'Bike',
-    price: 12700,
+    price: formatter.format(1270),
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Bicycle helmet',
-    price: 1200,
+    price: formatter.format(1200),
     image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Bicycle helmet',
-    price: 1600,
+    price: formatter.format(1600),
     image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Bicycle helmet',
-    price: 1500,
+    price: formatter.format(1500),
     image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Seat',
-    price: 300,
+    price: formatter.format(300),
     image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Tennis Montain Bike',
-    price: 2200,
+    price: formatter.format(2200),
     image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Sunglasses',
-    price: 800,
+    price: formatter.format(800),
     image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Sunglasses',
-    price: 600,
+    price: formatter.format(600),
     image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Bicycle seat bag',
-    price: 876,
+    price: formatter.format(876),
     image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 productList.push({
     name: 'Phone waterproof bag',
-    price: 876,
+    price: formatter.format(876),
     image: 'https://m.media-amazon.com/images/I/510VNwidLHL._AC_SY1000_.jpg',
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 function renderProducts(arr) {
@@ -136,7 +169,7 @@ function renderProducts(arr) {
         const productInfoDiv = document.createElement('div');
 
         const productPrice = document.createElement('p');
-        productPrice.innerText = `$${formatPrice(product.price)}`;
+        productPrice.innerText = product.price;
 
         const productName = document.createElement('p');
         productName.innerText = product.name;
@@ -161,10 +194,50 @@ function renderProducts(arr) {
 
 renderProducts(productList);
 
-function formatPrice(price) {
-    const numDecimalCommaSeparated = (Math.round(price * 100) / 100)
-        .toFixed(2)
-        .replace('.', ',');
+// Renderizando los detalles del producto seleccionado
 
-    return numDecimalCommaSeparated;
+const productCards = [...document.querySelectorAll('.product-card')];
+
+productCards.forEach((productCard) => {
+    productCard.addEventListener('click', renderCurrentItemDetails);
+});
+
+function renderCurrentItemDetails(e) {
+    productDetail.classList.remove('inactive');
+
+    const currentCardIndex = productCards.indexOf(e.currentTarget);
+
+    const currentItem = productList.find((product, productIndex) => {
+        if (productIndex == currentCardIndex) {
+            return product;
+        }
+    });
+
+    const slideImages = [...document.querySelectorAll('.slide-img')];
+    slideImages.forEach((img) => {
+        img.setAttribute('src', currentItem.image);
+        img.setAttribute('alt', currentItem.name);
+    });
+
+    const itemPrice = document.querySelector('#item-price');
+    itemPrice.innerText = currentItem.price;
+
+    const productName = document.querySelector('#item-name');
+    productName.innerText = currentItem.name;
+
+    const itemDescription = document.querySelector('#item-description');
+    itemDescription.innerText = currentItem.description;
+
+    automaticSlider();
+}
+
+function automaticSlider() {
+    let counter = 1;
+    setInterval(function () {
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if (counter > 3) {
+            counter = 1;
+        }
+    }, 3000);
 }
