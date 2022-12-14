8de26139eab1fc1c897deec5ee014144f3586fb8
Esteban Portillo <esteban.07.elias@gmail.com>
2022-12-14 08:48:17 -0600

    Creando archivo data.js para almacenar el array de productos

--- js/main.js
+++ js/main.js
@@ -42,11 +42,6 @@ function automaticSlider() {
     }, 3000);
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-});
-
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', showMobileMenu);
 closeButton.addEventListener('click', closeMobileMenu);
@@ -164,78 +159,6 @@ function exitProductDetail() {
     productDetail.classList.add('inactive');
     mainContainer.classList.remove('--product-detail-view');
 }
-
-const productList = [];
-productList.push({
-    name: 'Bike',
-    price: formatter.format(1270),
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Bicycle helmet',
-    price: formatter.format(1200),
-    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Bicycle helmet',
-    price: formatter.format(1600),
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Bicycle helmet',
-    price: formatter.format(1500),
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Seat',
-    price: formatter.format(300),
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Tennis Montain Bike',
-    price: formatter.format(2200),
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Sunglasses',
-    price: formatter.format(800),
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Sunglasses',
-    price: formatter.format(600),
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Bicycle seat bag',
-    price: formatter.format(876),
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-productList.push({
-    name: 'Phone waterproof bag',
-    price: formatter.format(876),
-    image: 'https://m.media-amazon.com/images/I/510VNwidLHL._AC_SY1000_.jpg',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
 
 function renderProducts(arr) {
     for (let product of arr) {
