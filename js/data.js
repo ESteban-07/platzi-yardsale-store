4778b29c06844e56a6bed8ecebc0eab88a6baa93
Esteban Portillo <esteban.07.elias@gmail.com>
2022-12-14 09:57:13 -0600

    Modificando las imagenes del array de productos

--- js/data.js
+++ js/data.js
@@ -6,81 +6,121 @@ const formatter = new Intl.NumberFormat('en-US', {
 const productList = [];
 
 productList.push({
-    name: 'Bike',
-    price: formatter.format(1270),
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-
-productList.push({
-    name: 'Bicycle helmet',
-    price: formatter.format(1200),
-    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
-    description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
-});
-
-productList.push({
-    name: 'Bicycle helmet',
+    name: 'Toddler Motocross',
     price: formatter.format(1600),
-    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg',
+    images: [
+        `https://m.media-amazon.com/images/I/71+M6q3kqRL._AC_SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/61gOjEoVXuL._AC_SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/71AR4SFpKKL._AC_SL1000_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Bicycle helmet',
+    name: 'Kids Helmet',
     price: formatter.format(1500),
-    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto',
+    images: [
+        `https://m.media-amazon.com/images/I/41vbxhmO97L._AC_.jpg`,
+        `https://m.media-amazon.com/images/I/41y3UeFUFVL._AC_.jpg`,
+        `https://m.media-amazon.com/images/I/41q1uibYJpL._AC_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Seat',
+    name: 'Motocross Boots',
     price: formatter.format(300),
-    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg',
+    images: [
+        `https://m.media-amazon.com/images/I/61QpAjpBxaL._AC_SL1080_.jpg`,
+        `https://m.media-amazon.com/images/I/71DP4FynbqS._AC_SL1080_.jpg`,
+        `https://m.media-amazon.com/images/I/61poxNfyfHL._AC_SL1080_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Tennis Montain Bike',
+    name: 'Mountain Bike',
+    price: formatter.format(1200),
+    images: [
+        `https://m.media-amazon.com/images/I/91DF+TfnkRL._SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/91VAholgrHL._SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/81ntWm93bSL._SL1500_.jpg`,
+    ],
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
+});
+
+productList.push({
+    name: 'Racing Glasses',
     price: formatter.format(2200),
-    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg',
+    images: [
+        `https://m.media-amazon.com/images/I/51hief6Qe8L._SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/51bpe5TewwL._SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/5133I-4YWbL._SL1000_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Sunglasses',
+    name: 'Toddler Bike',
+    price: formatter.format(1270),
+    images: [
+        `https://m.media-amazon.com/images/I/81MLY7dBRsL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71+tRvKUCSL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71lAOLqReQL._AC_SL1500_.jpg`,
+    ],
+    description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
+});
+
+productList.push({
+    name: 'Motocross Jersey ',
     price: formatter.format(800),
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602',
+    images: [
+        `https://m.media-amazon.com/images/I/71Ld+xaOAHL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71HEY1NCkRL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71NGdbZb3fL._AC_SL1500_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Sunglasses',
+    name: 'Racing Gloves',
     price: formatter.format(600),
-    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603',
+    images: [
+        `https://m.media-amazon.com/images/I/81aYx8kvBGL._AC_UL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/91Ym9pipEsL._AC_UL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71fce0p5B9L._AC_UL1500_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Bicycle seat bag',
+    name: 'Bike Seat',
     price: formatter.format(876),
-    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg',
+    images: [
+        `https://m.media-amazon.com/images/I/51No61TOgBL._AC_SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/61rhctt9tIL._AC_SL1000_.jpg`,
+        `https://m.media-amazon.com/images/I/51dVhVAMH0L._AC_SL1000_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
 
 productList.push({
-    name: 'Phone waterproof bag',
+    name: 'Waterproof Phone Bag',
     price: formatter.format(876),
-    image: 'https://m.media-amazon.com/images/I/510VNwidLHL._AC_SY1000_.jpg',
+    images: [
+        `https://m.media-amazon.com/images/I/81g4KlGJRUL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/71I8CfRwqyL._AC_SL1500_.jpg`,
+        `https://m.media-amazon.com/images/I/61BDyUDNycL._AC_SL1500_.jpg`,
+    ],
     description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum accusamus eius reprehenderit quos, perferendis vel!',
 });
